2d281e26a9b4cd8bb2072503faa539f5c4cf7bd8
Donya <dnyakrimi1020@gmail.com>
2023-09-30 14:35:57 -0700

    this is my first game

---
+++ main.py
@@ -0,0 +1,19 @@
+from logic import print_winner, is_draw, get_computer_choice, get_player_choice
+
+
+def play_game():
+    """play the game"""
+    player_choice = get_player_choice()
+    computer_choice = get_computer_choice()
+    if is_draw(player_choice, computer_choice):
+        print("It's a draw, both players picked %s: " % player_choice)
+    else:
+        print("Computer picked: %s" % computer_choice)
+        print("Player picked: %s" % player_choice)
+        print_winner(player_choice, computer_choice)
+
+
+if __name__ == "__main__":
+    play_game()
+
+
